7de6a4728cd51e17fe906b6d51849ff3e2aab77e
KarthikS <38109484+karthikskmurthy@users.noreply.github.com>
2021-05-23 00:19:09 +0530

    updated cypher query

--- src/api/constants.py
+++ src/api/constants.py
@@ -13,7 +13,7 @@ MERGE (c)-[: dag]->(d1)
 MERGE(d2:DAG{{{0}:'{2}'}})
 with c, d1, d2
 MERGE (c)-[: dag]->(d2)
-MERGE (d2)-[: depends_on]->(d1)
+MERGE (d1)-[: depends_on]->(d2)
 """
 QUERY_NON_DEPENDENCY = """
 MERGE(c:company{{{0}:'abc'}})
